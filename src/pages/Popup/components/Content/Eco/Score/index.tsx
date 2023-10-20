a31d9394c9c63d73a67b2975669a6248f3ada9ad
Antoine Kingue <contact@antoinek.fr>
2023-10-20 20:26:00 +0200

    🎨 add score and about

---
+++ src/pages/Popup/components/Content/Eco/Score/index.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export enum SmileyType {
+  Happy = 'happy',
+  Good = 'good',
+  Sad = 'sad',
+  Smile = 'smile',
+}
+
+const Score: React.FC = () => {
+  return (
+    <Container>
+      <EcoIndex>
+        <Smiley
+          src={require(`../../../../../../assets/img/smileys/${SmileyType.Happy}.svg`)}
+          alt={SmileyType.Happy}
+        />
+        <Note>93/100</Note>
+      </EcoIndex>
+      <Content>
+        <Title>Youpi! onRuntime est classé “A”</Title>
+        <Description>Calculé la dernière fois le : 18/10/2023</Description>
+      </Content>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 15px 0;
+  gap: 10px;
+`;
+
+const Smiley = styled.img`
+  height: 35px;
+  width: 35px;
+`;
+
+const EcoIndex = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Note = styled.div`
+  font-size: 30px;
+  font-weight: 600;
+  color: #009245;
+  font-family: 'neulis-cursive';
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`;
+
+const Title = styled.div`
+  font-size: 15px;
+  font-weight: 600;
+  color: #014335;
+`;
+
+const Description = styled.div`
+  font-size: 10px;
+  color: #909090;
+`;
+
+export default Score;
--- src/pages/Popup/components/Content/Eco/index.tsx
+++ src/pages/Popup/components/Content/Eco/index.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import Activity from './Activity';
 import Report from './Report';
+import Score from './Score';
+import styled from 'styled-components';
 
 const Eco: React.FC = () => {
   return (
     <>
       <Activity />
+      <Score />
       <Report />
+
+      <About>
+        Envie d’en savoir plus?{' '}
+        <a
+          href={'https://onruntime.com'}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Cliquez ici
+        </a>
+      </About>
     </>
   );
 };
+
+const About = styled.div`
+  font-size: 10px;
+  color: #909090;
+
+  a {
+    color: #009245;
+    font-weight: 600;
+  }
+`;
 
 export default Eco;
