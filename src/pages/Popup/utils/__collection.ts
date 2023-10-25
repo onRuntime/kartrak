e8e9e1e986fd6a890b9eb5774a8725f0d902ec37
Antoine Kingue <contact@antoinek.fr>
2023-10-25 13:03:50 +0200

    ✨ add random number in collection utils

--- src/pages/Popup/utils/__collection.ts
+++ src/pages/Popup/utils/__collection.ts
@@ -136,3 +136,7 @@ export const getSmileyType = (ecoIndex?: number) => {
             ? SmileyType.Sad
             : SmileyType.Bad;
 };
+
+export const getRandomNumber = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
