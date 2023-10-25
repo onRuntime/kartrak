c7ab939066be9263a7923892dacb884b6c46f08d
Lucas Bodin <contact.imazyx@gmail.com>
2023-10-25 23:49:05 +0200

    🔧 update datas in tips

--- src/pages/Popup/data/tips.ts
+++ src/pages/Popup/data/tips.ts
@@ -1,6 +1,25 @@
 const tips = [
   "Économisez de l'énergie en réduisant la luminosité de votre écran lorsque vous naviguez sur le web. Vous prolongerez la durée de vie de votre appareil tout en contribuant à la réduction de votre empreinte carbone.",
-  "Éteignez votre ordinateur lorsque vous ne l'utilisez pas. Vous économiserez de l'énergie et prolongerez la durée de vie de votre appareil."
+  "Éteignez votre ordinateur lorsque vous ne l'utilisez pas. Vous économiserez de l'énergie et prolongerez la durée de vie de votre appareil.",
+  "Débranchez vos chargeurs de l'appareil et de la prise lorsque la batterie est chargée. Ils consomment de l'énergie même lorsque votre appareil n'est pas connecté.",
+  "Optez pour des moteurs de recherche écoresponsables qui compensent leurs émissions de CO2 et utilisent des serveurs alimentés par des énergies renouvelables.",
+  "Éteignez vos ordinateurs, tablettes et téléphones lorsque vous ne les utilisez pas. Le mode veille consomme de l'énergie.",
+  "Supprimez les e-mails inutiles et désabonnez-vous des newsletters non désirées pour réduire la charge énergétique des serveurs de messagerie.",
+  "Les publicités en ligne peuvent être énergivores. Les bloqueurs de publicités réduisent la consommation de données et l'énergie nécessaire pour les afficher.",
+  "Optimisez votre navigation en fermant les onglets et les applications inutilisés pour réduire la charge de travail de votre processeur.",
+  "Baissez la résolution des vidéos en streaming pour économiser la bande passante et l'énergie.",
+  "La nuit, désactivez le Wi-Fi lorsque vous ne l'utilisez pas pour économiser de l'énergie.",
+  "Nettoyez et supprimez les fichiers inutiles de votre boîte de téléchargement pour libérer de l'espace de stockage.",
+  "N'envoyez pas de pièces jointes volumineuses par courrier électronique. Utilisez des services de stockage en ligne pour partager des fichiers.",
+  "Les mises à jour de logiciels peuvent améliorer l'efficacité énergétique de votre appareil.",
+  "Limitez la quantité de données que les applications et les sites web collectent à votre sujet.",
+  "Videz régulièrement le cache et les cookies de votre navigateur pour améliorer les performances et réduire la quantité de données stockées.",
+  "Encouragez le travail à distance pour réduire les déplacements, économiser de l'énergie et réduire les émissions de CO2 liées aux trajets domicile-travail.",
+  "Téléchargez des contenus pour une lecture hors ligne lorsque cela est possible, ce qui réduit la nécessité de se connecter à Internet fréquemment.",
+  "Limitez la fréquentation de sites web riches en animations et publicités énergivores.",
+  "Optez pour des polices de caractères qui consomment moins d'énergie à l'affichage.",
+  "Utilisez des extensions (comme nous) pour gérer les onglets de votre navigateur, réduisant ainsi l'utilisation de la mémoire et du processeur.",
+  "Soutenez et promouvez des entreprises et des projets qui s'engagent activement dans des pratiques numériques durables."
 ]
 
 export default tips;
