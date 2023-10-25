0161fe4fecfb9b959022d64040899718968fb7e5
Antoine Kingue <contact@antoinek.fr>
2023-10-25 13:03:16 +0200

    🍱 add tips data

---
+++ src/pages/Popup/data/tips.ts
@@ -0,0 +1,6 @@
+const tips = [
+  "Économisez de l'énergie en réduisant la luminosité de votre écran lorsque vous naviguez sur le web. Vous prolongerez la durée de vie de votre appareil tout en contribuant à la réduction de votre empreinte carbone.",
+  "Éteignez votre ordinateur lorsque vous ne l'utilisez pas. Vous économiserez de l'énergie et prolongerez la durée de vie de votre appareil."
+]
+
+export default tips;
